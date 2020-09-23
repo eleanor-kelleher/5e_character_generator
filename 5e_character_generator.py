2e4fb7491f41c63179de9fd3727770d2660d76e0
James Kelleher <thisisjameskelleher@gmail.com>
2020-09-23 11:23:43 +0100

    Cleaned up code. Organised functions and added argument typing.

--- 5e_character_generator.py
+++ 5e_character_generator.py
@@ -7,6 +7,10 @@ import random
 # STATICS
 BLANK_CHAR_SHEET = 'Character Sheet - Form Fillable.pdf'
 OUTPUT_CHAR_SHEET = 'Character_Sheet_Output.pdf'
+RACE_PATH = 'phbdata/races.json'
+BACKGROUND_PATH = 'phbdata/backgrounds.json'
+CLASS_PATH = 'phbdata/classes.json'
+ITEM_PATH = 'phbdata/items.json'
 PROF = 2
 ALIGNMENT = ['LG', 'NG', 'CG', 'LN', 'N', 'CN', 'LE', 'NE', 'CE']
 ANNOT_KEY = '/Annots'           # key for all annotations within a page
@@ -31,41 +35,36 @@ LANGUAGES = ["Common", "Dwarvish", "Elvish", "Giant", "Gnomish", "Goblin", "Half
              "Primordial", "Sylvan", "Undercommon"]
 
 
-def d6():
+def load_json(file_path: str) -> dict:
+    with open(file_path) as file:
+        data_dict = json.loads(file.read())
+        return data_dict
+
+
+def d6() -> int:
     return random.randint(1, 6)
 
 
-def drop_lowest(roll_list):
-    min_stat = roll_list[0]
+def drop_lowest(rolls: list) -> list:
+    min_stat = rolls[0]
     min_i = 0
-    for i, roll in enumerate(roll_list):
+    for i, roll in enumerate(rolls):
         if roll < min_stat:
             min_stat = roll
             min_i = i
-    del roll_list[min_i]
-    return roll_list
-
-
-def drop_lowest_in_dict(roll_list):
-    min_stat = roll_list[0]
-    min_i = 0
-    for i, roll in enumerate(roll_list):
-        if roll < min_stat:
-            min_stat = roll
-            min_i = i
-    del roll_list[min_i]
-    return roll_list
+    del rolls[min_i]
+    return rolls
 
 
 # convention is 4d6, drop the lowest
-def roll_one_stat():
+def roll_one_stat() -> int:
     all_rolls = [d6(), d6(), d6(), d6()]
     good_rolls = drop_lowest(all_rolls)
     return sum(good_rolls)
 
 
 # takes 7 stats, keeps highest 6
-def generate_stats():
+def generate_stats() -> dict:
     seven_stats = [roll_one_stat(), roll_one_stat(), roll_one_stat(), roll_one_stat(),
                    roll_one_stat(), roll_one_stat(), roll_one_stat()]
     six_stats = drop_lowest(seven_stats)
@@ -77,7 +76,17 @@ def generate_stats():
             'CHA': six_stats[5]}
 
 
-def add_racial_mods(stats, mods):
+def get_modifier(stat: int, proficiency: int = 0) -> int:
+    return math.floor((stat - 10) / 2) + proficiency
+
+
+def plus_or_minus(mod: int) -> str:
+    if mod >= 0:
+        return '+' + str(mod)
+    return str(mod)
+
+
+def add_racial_mods(stats: dict, mods: dict) -> dict:
     for stat_name in stats:
         if stat_name in mods:
             stats[stat_name] = stats[stat_name] + mods[stat_name]
@@ -91,81 +100,13 @@ def add_racial_mods(stats, mods):
                 stats[current_stat] = stats[current_stat] + 1
                 already_added.append(current_stat)
                 count = count - 1
+    return stats
 
 
-def random_languages(count, already_known):
-    lang_list = []
-    while count > 0:
-        current_lang = random.choice(LANGUAGES)
-        if current_lang not in already_known:
-            lang_list.append(current_lang)
-            count = count - 1
-    return lang_list
-
-
-def random_race():
-    races_file = open('phbdata/races.json')
-    races_dict = json.loads(races_file.read())
-    race = random.choice(list(races_dict))
-    print(race)
-    race_attributes = races_dict[race]
-    char_data['Race'] = race
-    char_data['Speed'] = race_attributes['speed']
-
-    languages.extend(race_attributes['languages'])
-    if 'extra_languages' in race_attributes:
-        languages.extend(random_languages(race_attributes['extra_languages'], languages))
-    racial_modifiers = race_attributes['mods']
-    if race_attributes['darkvision'] == 'yes':
-        features.append('Darkvision')
-    if "Breath Weapon" in race_attributes:
-        # TODO
-        pass
-    if 'features' in race_attributes:
-        for feature, description in race_attributes['features'].items():
-            string = feature + ". " + description
-            features.append(string)
-    races_file.close()
-    return racial_modifiers
-
-
-def random_background():
-    bg_file = open('phbdata/backgrounds.json')
-    bg_dict = json.loads(bg_file.read())
-    background = random.choice(list(bg_dict))
-    bg_attributes = bg_dict[background]
-
-    char_data['Background'] = background
-    equipment.extend(choose_item_option(bg_attributes['equipment']))
-    char_data['GP'] = bg_attributes['gp']
-
-    char_data['PersonalityTraits'] = random.choice(bg_attributes['trait'])
-    char_data['Ideals'] = random.choice(bg_attributes['ideal'])
-    char_data['Bonds'] = random.choice(bg_attributes['bond'])
-    char_data['Flaws'] = random.choice(bg_attributes['flaw'])
-
-    if 'languages' in bg_attributes:
-        count = bg_attributes['languages']
-        languages.extend(random_languages(count, languages))
-
-    skills.extend(bg_attributes['skills'])
-    bg_file.close()
-
-
-def get_modifier(stat, proficiency=0):
-    return math.floor((stat - 10) / 2) + proficiency
-
-
-def plus_or_minus(mod):
-    if mod >= 0:
-        return '+' + str(mod)
-    return str(mod)
-
-
-def get_ability_modifiers():
+def get_ability_modifiers(stats: dict) -> list:
     ability_modifiers = []
     mod_name = ''
-    for stat_name, stat_val in stat_dict.items():
+    for stat_name, stat_val in stats.items():
         if stat_name == 'CHA':
             mod_name = 'CHamod'
         else:
@@ -178,33 +119,125 @@ def get_ability_modifiers():
     return ability_modifiers
 
 
-def get_skill_modifiers(prof_skills, ability_modifiers, proficiency):
+def get_skill_modifiers(skills: list, ability_modifiers: list, proficiency: int) -> dict:
+    skill_modifiers = dict()
     for i, mod in enumerate(ability_modifiers):
         for button, skill_name in SKILL_LIST[i].items():
             if skill_name in skills:
-                char_data[skill_name] = plus_or_minus(ability_modifiers[i] + proficiency)
+                skill_modifiers[skill_name] = plus_or_minus(ability_modifiers[i] + proficiency)
                 proficiency_button = 'Check Box ' + button
-                char_data[proficiency_button] = 'Yes'
+                skill_modifiers[proficiency_button] = 'Yes'
                 if skill_name == 'Perception':
-                    char_data['Passive'] = 10 + ability_modifiers[i] + proficiency
+                    skill_modifiers['Passive'] = 10 + ability_modifiers[i] + proficiency
             else:
-                char_data[skill_name] = plus_or_minus(ability_modifiers[i])
+                skill_modifiers[skill_name] = plus_or_minus(ability_modifiers[i])
                 if skill_name == 'Perception':
-                    char_data['Passive'] = 10 + ability_modifiers[i]
+                    skill_modifiers['Passive'] = 10 + ability_modifiers[i]
+    return skill_modifiers
 
 
-def choose_item_option(item_list):
+def select_item_option(items: list) -> list:
     final_list = []
-    for item in item_list:
+    for item in items:
         if '/' in item:
             item_options = item.split('/')
-            final_list.append(random.choice(item_options))
+            item_choice = random.choice(item_options)
+            if item_choice == "artisan":
+                final_list.append(random.choice(item_dict['artisan']))
+            elif item_choice == "instrument":
+                final_list.append(random.choice(item_dict['instrument']))
         else:
-            final_list.append(item)
+            if item == "artisan":
+                x = random.choice(item_dict['artisan'])
+                final_list.append(x)
+            elif item == "instrument":
+                final_list.append(random.choice(item_dict['instrument']))
     return final_list
 
 
-def write_fillable_pdf(input_pdf_path, output_pdf_path, data_dict):
+def select_languages(count: int, already_known: list):
+    lang_list = []
+    while count > 0:
+        current_lang = random.choice(LANGUAGES)
+        if current_lang not in already_known:
+            lang_list.append(current_lang)
+            count = count - 1
+    return lang_list
+
+
+def select_race(races: dict, data: dict, features: list, languages: list) -> dict:
+    race = random.choice(list(races_dict))
+    race_attributes = races_dict[race]
+
+    data['Race'] = race
+    data['Speed'] = race_attributes['speed']
+    racial_modifiers = race_attributes['mods']
+
+    languages.extend(race_attributes['languages'])
+    if 'extra_languages' in race_attributes:
+        languages.extend(select_languages(race_attributes['extra_languages'], languages))
+
+    if race_attributes['darkvision'] == 'yes':
+        features.append('Darkvision')
+    if "Breath Weapon" in race_attributes:
+        # TODO
+        pass
+    if 'features' in race_attributes:
+        for feature, description in race_attributes['features'].items():
+            string = feature + ". " + description
+            features.append(string)
+
+    return racial_modifiers
+
+
+def select_background(backgrounds: dict, data: dict, skills: list, equipment: list, languages: list):
+    background = random.choice(list(bg_dict))
+    bg_attributes = bg_dict[background]
+    char_data['Background'] = background
+    equipment.extend(select_item_option(bg_attributes['equipment']))
+    char_data['GP'] = bg_attributes['gp']
+
+    char_data['PersonalityTraits'] = random.choice(bg_attributes['trait'])
+    char_data['Ideals'] = random.choice(bg_attributes['ideal'])
+    char_data['Bonds'] = random.choice(bg_attributes['bond'])
+    char_data['Flaws'] = random.choice(bg_attributes['flaw'])
+
+    if 'languages' in bg_attributes:
+        count = bg_attributes['languages']
+        languages.extend(select_languages(count, languages))
+
+    skills.extend(bg_attributes['skills'])
+
+
+def select_class(stats: dict, char_data: dict, skills: list, proficiencies: list, equipment: list):
+    player_class = random.choice(list(class_dict))
+    class_attributes = class_dict[player_class]
+    print(player_class)
+
+    char_data['ClassLevel'] = player_class + ' 1'
+    char_data['HD'] = 'd' + str(class_attributes['hd'])
+    char_data['HDTotal'] = '1'
+    char_data['HPTemp'] = '0'
+
+    i = 0
+    while i < class_attributes['skill number']:
+        skill = random.choice(class_attributes['skills'])
+        if skill not in skills:
+            skills.append(skill)
+            i = i + 1
+    skills.extend(class_attributes['saves'])
+
+    ability_modifiers = get_ability_modifiers(stats)
+    char_data.update(get_skill_modifiers(skills, ability_modifiers, PROF))
+    hit_points = class_attributes['hd'] + ability_modifiers[2]
+    char_data['HPMax'] = hit_points
+    char_data['HPCurrent'] = hit_points
+
+    proficiencies.extend(select_item_option(class_attributes['prof']))
+    equipment.extend(select_item_option(class_attributes['equipment']))
+
+
+def write_fillable_pdf(input_pdf_path: str, output_pdf_path: str, data_dict: dict):
     template_pdf = pdfrw.PdfReader(input_pdf_path)
 
     annotations = template_pdf.pages[0][ANNOT_KEY]
@@ -232,42 +265,22 @@ if __name__ == '__main__':
     features = []
 
     char_data['CharacterName'] = names.get_first_name()
-    char_data['ProfBonus'] = '+' + str(PROF)
+    char_data['ProfBonus'] = plus_or_minus(PROF)
     char_data['Alignment'] = random.choice(ALIGNMENT)
     char_data['XP'] = 0
 
-    racial_mods = random_race()
+    # load json dictionaries
+    races_dict = load_json(RACE_PATH)
+    bg_dict = load_json(BACKGROUND_PATH)
+    class_dict = load_json(CLASS_PATH)
+    item_dict = load_json(ITEM_PATH)
 
-    stat_dict = generate_stats()
-    add_racial_mods(stat_dict, racial_mods)
-
-    random_background()
-
-    # select_class(stat_dict)
-    class_file = open('phbdata/classes.json')
-    class_dict = json.loads(class_file.read())
-    player_class = random.choice(list(class_dict))
-    class_attributes = class_dict[player_class]
-
-    char_data['ClassLevel'] = player_class + ' 1'
-    char_data['HD'] = 'd' + str(class_attributes['hd'])
-    char_data['HDTotal'] = '1'
-    char_data['HPTemp'] = '0'
-
-    skills.extend(random.sample(class_attributes['skills'], class_attributes['skill number']))
-    skills.extend(class_attributes['saves'])
-    print(skills)
-
-    ability_modifiers = get_ability_modifiers()
-    get_skill_modifiers(skills, ability_modifiers, PROF)
-    hit_points = class_attributes['hd'] + ability_modifiers[2]
-    char_data['HPMax'] = hit_points
-    char_data['HPCurrent'] = hit_points
-
-    proficiencies.extend(choose_item_option(class_attributes['prof']))
-    equipment.extend(choose_item_option(class_attributes['equipment']))
-
-    char_data.update(stat_dict)
+    racial_mods = select_race(races_dict, char_data, features, languages)
+    select_background(bg_dict, char_data, skills, equipment, languages)
+    stats = generate_stats()
+    stats = add_racial_mods(stats, racial_mods)
+    char_data.update(stats)
+    select_class(stats, char_data, skills, proficiencies, equipment)
 
     # fill out proficiencies & languages
     char_data['ProficienciesLang'] = ', '.join(languages) + '\n\n' + ', '.join(proficiencies)
